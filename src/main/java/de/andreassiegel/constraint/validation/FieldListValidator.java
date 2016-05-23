67028d50fe6efe04bca2c265872c67e2d2955bf5
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-05-23 16:06:22 +0200

    Set property path in constraint violation(s)

--- src/main/java/de/andreassiegel/constraint/validation/FieldListValidator.java
+++ src/main/java/de/andreassiegel/constraint/validation/FieldListValidator.java
@@ -5,6 +5,9 @@ import de.andreassiegel.constraint.ExclusiveField;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class FieldListValidator implements ConstraintValidator<ExclusiveField, Object> {
@@ -19,35 +22,63 @@ public class FieldListValidator implements ConstraintValidator<ExclusiveField, O
     @Override
     public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
 
+        constraintValidatorContext.disableDefaultConstraintViolation();
+
         if (fields.length > 1) {
 
-            Boolean fieldSet = false;
+            List<String> setFields = definedFields(object);
 
-            for (String fieldName : fields) {
-                try {
-                    Field field = object.getClass().getDeclaredField(fieldName);
-                    field.setAccessible(true);
-                    Object value = field.get(object);
+            if (setFields.size() == 1) {
+                return true;
+            } else {
+                String message = constraintViolationMessage(setFields);
 
-                    if (!isNullOrEmpty(value)) {
-                        if (fieldSet) {
-                            return false;
-                        } else {
-                            fieldSet = true;
-                        }
-                    }
-                } catch (NoSuchFieldException e) {
-                    System.out.println("The field '" + fieldName + "' declared for validation does not exist in class '" + object.getClass().getName() + "'");
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
+                for (String fieldName : setFields) {
+                    constraintValidatorContext.buildConstraintViolationWithTemplate(message)
+                            .addPropertyNode(fieldName)
+                            .addConstraintViolation();
                 }
-            }
 
-            return fieldSet;
+                return false;
+            }
         }
 
         return true;
+    }
+
+    private String constraintViolationMessage(List<String> setFields) {
+        String message;
+
+        if (setFields.isEmpty()) {
+            message = "one of " + Arrays.toString(fields) + " may not be undefined";
+        } else {
+            message = "only a single field of " + Arrays.toString(fields) + " may be defined";
+        }
+
+        return message;
+    }
+
+    private List<String> definedFields(Object object) {
+        List<String> setFields = new ArrayList<String>();
+
+        for (String fieldName : fields) {
+            try {
+                Field field = object.getClass().getDeclaredField(fieldName);
+                field.setAccessible(true);
+                Object value = field.get(object);
+
+                if (!isNullOrEmpty(value)) {
+                    setFields.add(fieldName);
+                }
+            } catch (NoSuchFieldException e) {
+                System.out.println("The field '" + fieldName + "' declared for validation does not exist in class '" + object.getClass().getName() + "'");
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return setFields;
     }
 
     private boolean isNullOrEmpty(Object value) {
